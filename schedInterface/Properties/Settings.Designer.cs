37d5dcdb3441abf127ad0abc98a422f2164f5e7e
Diego Dindurra <diego@tipit.net>
2018-03-28 10:50:29 -0300

    Managing 2018 overlays.

--- schedInterface/Properties/Settings.Designer.cs
+++ schedInterface/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace schedInterface.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -27,7 +27,7 @@ namespace schedInterface.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=172.16.1.65\\FNTECH;Initial Catalog=fntech_signage;Persist Security In" +
-            "fo=True;User ID=sa;Password=OhHowHeLovesUs!")]
+            "fo=True;User ID=sa")]
         public string fntech_signageConnectionString {
             get {
                 return ((string)(this["fntech_signageConnectionString"]));
@@ -42,6 +42,27 @@ namespace schedInterface.Properties {
         public string fntech_signageConnectionString1 {
             get {
                 return ((string)(this["fntech_signageConnectionString1"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=fntech_signage;User ID=sa")]
+        public string fntech_signageConnectionString2 {
+            get {
+                return ((string)(this["fntech_signageConnectionString2"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=fntech_signage;Persist Security Info=True;U" +
+            "ser ID=sa")]
+        public string fntech_signageConnectionString3 {
+            get {
+                return ((string)(this["fntech_signageConnectionString3"]));
             }
         }
     }
